c750b1d8a1b35adfee081099e794ce987fd1f002
Akira Noda <akira.noda@onecareer.com>
2022-12-02 17:56:50 +0900

    ascdasd

--- ssr/nuxt.config.js
+++ ssr/nuxt.config.js
@@ -13,7 +13,31 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    ],
+    script: [
+      {
+        hid: 'gtmHead',
+        innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T8JKG8R');'`
+      }
+    ],
+    noscript: [
+      {
+        // NOTE: This code parsed as string in HTML when the browser can use Javascript.
+        // If you disble Javascirpt then check the page, this code parsed as tag!
+        hid: 'gtmBody',
+        innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T8JKG8R"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        body: true
+      }
+    ],
+    __dangerouslyDisableSanitizersByTagID: {
+      'gtmHead': ['innerHTML'],
+      'gtmBody': ['innerHTML']
+    }
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
