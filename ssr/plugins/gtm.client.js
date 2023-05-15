ed829da033fe12633f386c74348a9488b446c068
Akira Noda <akira.noda@onecareer.com>
2023-05-15 20:41:20 +0900

    add plugin

--- ssr/plugins/gtm.client.js
+++ ssr/plugins/gtm.client.js
@@ -1,9 +1,10 @@
 export default async ({ app }) => {
-  app.router.afterEach(() => {
+  app.router.afterEach(async () => {
     if (window.dataLayer === undefined) {
       window.dataLayer = [];
     }
+    await new Promise(resolve => setTimeout(resolve, 500))
     window.dataLayer.push({ user_id: 123, event: 'login' });
-    // window.dataLayer.push({ 'event': 'setUserID', user_id: 123 });
-  });
+  })
+  // window.dataLayer.push({ 'event': 'setUserID', user_id: 123 });
 };
